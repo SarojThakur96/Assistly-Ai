98b6496fa131e2f7d8332ada527192b909cc3630
Saroj Kumar Thakur <saroj@distronix.in>
2025-05-19 19:12:13 +0530

    create ai bot and seeds added

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Sidebar from "@/components/sidebar";
 import React from "react";
 
 const AdminLayout = ({
@@ -14,6 +15,10 @@ const AdminLayout = ({
 
       <div className="flex flex-col flex-1 lg:flex-row bg-gray-100">
         {/* sidebar */}
+
+        <Sidebar />
+
+        {/* main content */}
         <div className="flex flex-1 justify-center lg:justify-start items-start max-w-5xl mx-auto w-full">
           {children}
         </div>
