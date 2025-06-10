c1fdc4c0d687c598f3a680c7fbfb418932afe55b
Saroj Kumar Thakur <saroj@distronix.in>
2025-06-10 13:30:26 +0530

    gradient text added in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import Avatar from "./Avatar";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import GradientText from "./GradientText";
 
 const Header = () => {
   return (
@@ -9,8 +10,17 @@ const Header = () => {
       <Link href="/" className="flex items-center text-4xl font-thin gap-2">
         <Avatar seed="Distronix Chat Agent" />
         <div className="space-y-1">
-          <h1>Promptly AI</h1>
-          <h2 className="text-sm">Build & Share Your Custom AI Assistants</h2>
+          <GradientText
+            colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+            animationSpeed={3}
+            showBorder={false}
+            className="ml-0"
+          >
+            <h1>Promptly AI</h1>
+          </GradientText>
+          <h2 className="text-sm font-semibold">
+            Build & Share Your Custom AI Assistants
+          </h2>
         </div>
       </Link>
 
